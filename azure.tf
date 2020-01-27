31caabe0e52166d35136a129b9d81c16a8b7419c
Ojas Kale <ojasok@gmail.com>
2020-01-28 01:01:33 +0530

    ingress folder

--- azure.tf
+++ azure.tf
@@ -10,6 +10,15 @@ variable "container_name" {
   type = string
 }
 
+# module "ingress_kube_cluster" {
+#   source = "./ingress-kube/"
+#   aks_service_principal_app_id="d90eb49b-6418-4335-acdb-f3d74c44b0ab"
+#   aks_service_principal_client_secret="f715d4b6-429a-4cd9-a54a-e02042d66097"
+#   aks_service_principal_object_id="86266283-c3fe-4557-8440-d372fe4e525d"
+#   resource_group_name="CLIGroupForArm"
+#   location="westus"
+# }
+
 module "backend" {
   source = "./terraform-backend/"
 }
---
+++ ingress-kube/rg.tf
@@ -0,0 +1,13 @@
+data "azurerm_resource_group" "rg" {
+  name = var.resource_group_name
+}
+
+# User Assigned Identities 
+resource "azurerm_user_assigned_identity" "testIdentity" {
+  resource_group_name = data.azurerm_resource_group.rg.name
+  location            = data.azurerm_resource_group.rg.location
+
+  name = "identity1"
+
+  tags = var.tags
+}
