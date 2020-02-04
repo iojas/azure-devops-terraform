73cf05f4d997d06b2f6f50d306c43d2f77b57ba0
Ojas Kale <ojasok@gmail.com>
2020-02-04 17:07:01 +0530

    should work out of the box

--- postgres.tf
+++ postgres.tf
@@ -23,14 +23,22 @@ resource "azurerm_postgresql_server" "example" {
   administrator_login          = var.POSTGRES_USERNAME
   administrator_login_password = var.POSTGRES_PASSWORD
   version                      = "9.5"
-  ssl_enforcement              = "Enabled"
+  ssl_enforcement              = "Disabled"
 }
 
 
-resource "azurerm_postgresql_firewall_rule" "example" {
+resource "azurerm_postgresql_firewall_rule" "openrule" {
   name                = "openrule"
   resource_group_name = data.azurerm_resource_group.rg.name
   server_name         = azurerm_postgresql_server.example.name
   start_ip_address    = "0.0.0.0"
   end_ip_address      = "255.255.255.255"
+}
+
+resource "azurerm_postgresql_firewall_rule" "example" {
+  name                = "openrule2"
+  resource_group_name = data.azurerm_resource_group.rg.name
+  server_name         = azurerm_postgresql_server.example.name
+  start_ip_address    = "0.0.0.0"
+  end_ip_address      = "0.0.0.0"
 }
